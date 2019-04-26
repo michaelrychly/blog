f09c0343601783f43f0ad1b390cc8a094ebcf135
michaelrychly <MichaelRychly@gmail.com>
2019-04-26 01:02:06 -0400

    editing and deleting an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -12,8 +16,24 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   def index
